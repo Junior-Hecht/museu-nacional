0c282d191e426d91b5c787a6d71bd94a36dd67ff
Júnior. Hecht.W <juniorhechtjr@gmail.com>
2021-07-18 22:24:46 -0300

    Criado layout de landing-page

---
+++ OneDrive/Área de Trabalho/VETEX-HIRING-CODERS/landing-page/js/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+ e.preventDefault();
+    let nome = document.getElementById('nome').value;                  
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+}
+let convertData = JSON.stringify(data);
+
+localStorage.setItem('lead', convertData)
+
+let content = document.getElementById('content')
+
+let carregando = '<p>carregando...</p>'
+
+let Ok = '<p>Ok</p>'
+
+content.innerHTML = carregando
+
+setTimeout(() => {
+   content.innerHTML = Ok
+}, 1000)
+
+})
+
+
